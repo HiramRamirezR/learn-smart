748da8e8e0f649ea5b9ae36891753daa77f4c5f6
hiramramirez <hiram.j.ramirez@gmail.com>
2023-05-25 02:54:45 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const contactForm = document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita el envío del formulario
+
+  // Obtiene los valores ingresados en los campos del formulario
+  const name = contactForm.elements.name.value;
+  const email = contactForm.elements.email.value;
+  const phone = contactForm.elements.phone.value;
+
+  // Muestra una alerta con los datos ingresados
+  alert(`Nombre: ${name}\nCorreo electrónico: ${email}\nTeléfono: ${phone}`);
+
+  // Restablece el formulario
+  contactForm.reset();
+});
